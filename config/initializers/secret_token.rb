a7fb06228bb02c458dd8261a3cc47160308152b6
Johan Muir <johanmuir@gmail.com>
2011-04-21 14:40:20 -0600

    adding initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sincesar::Application.config.secret_token = '1ea9c26ec996e40b76c316a6dd3f4915d2c1bcbd5d2a16931e77908205560545f3acddc17bb480f45f663894d3ab11462be087390d36f653b5fe4b8502cdcb4a'
